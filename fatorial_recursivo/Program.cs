4c7afe6d3fb6abcd2aea56191e9b91dd04a83a4f
Arthur Pimentel <arthur.pimentel@perlink.net>
2021-07-08 11:24:20 -0300

    Adicionar arquivos de projeto.

---
+++ fatorial_recursivo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace fatorial_recursivo
+{
+    class Fatorial
+    {
+        private int i; //numero a ser calculado o fatorial
+        private int fat;//resultado do calculo
+        public Fatorial(int n)
+        {
+            Console.WriteLine("Calculando fatorial...");
+            i = n;
+            fat = getFatorial(i);
+        }
+        public void setNumero(int n)
+        {
+            i = n;
+        }
+        public int getFatorial(int n)
+        {
+            int temp;
+            if (n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                temp = n * getFatorial(n - 1);
+                return temp;
+            }
+        }
+        public void mostrar()
+        {
+            Console.WriteLine("numero: {0}, fatorial: {1}\n", i, fat);
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            int i = Convert.ToInt32(Console.ReadLine());
+            Fatorial fat = new Fatorial(i);
+            fat.mostrar();
+        }
+    }
+}
